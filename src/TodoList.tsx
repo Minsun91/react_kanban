a5c4aba26f7d74e5249dafc6582d246627c210e3
Minsun91 <minsunkim167@gmail.com>
2022-12-28 15:28:38 +0100

    useForm 2번째

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,139 +1,120 @@
-import React from "react";
 import { useForm } from "react-hook-form";
 
-// function ToDoList() {
-//     const [toDo, setTodo] = useState("");
-//     const [todoError, setTodoError] = useState("");
-
-//     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//         const {
-//             currentTarget: { value },
-//         } = event;
-//         setTodoError("");
-//         setTodo(value);
-//     };
-//     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//         event.preventDefault();
-//         if (toDo.length < 10) {
-//             return setTodoError("To Do should be longer than that");
-//         }
-//         console.log("submit");
-//     };
-//     return (
-//         <div>
-//             <form onSubmit={onSubmit}>
-//                 <input
-//                     onChange={onChange}
-//                     value={toDo}
-//                     placeholder="Write to do"
-//                 />
-//                 <button>Add</button>
-//                 {todoError !== "" ? todoError : null}
-//             </form>
-//         </div>
-//     );
-// }
-
 interface IForm {
-    Email: string;
-    LastName: string;
-    FirstName: string;
-    Password1: string;
-    Password2: string;
-    extraError?: string;
+    toDo:string;
 }
 
 function ToDoList() {
-    //handleSubmit is for validation and preventDefault
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        setError,
-    } = useForm<IForm>({
-        defaultValues: {
-            Email: "@gmail.com",
-        },
-    });
-    const onValid = (data: IForm) => {
-        if (data.Password1 !== data.Password2) {
-            setError(
-                "Password1",
-                { message: "Password are not the same." },
-                { shouldFocus: true }
-            ); //password1에서 에러 나면 커서가 이리고 옮겨진다.
-            setError("extraError", { message: "Server offline" });
-        }
-    };
-    console.log(errors);
-
+    const {register, handleSubmit, setValue} = useForm<IForm>()
+    const handleValid = (data:IForm)=>{
+        console.log("add to do", data.toDo)
+        setValue("toDo", "")  //
+    }
     return (
         <div>
-            <form
-                style={{ display: "flex", flexDirection: "column" }}
-                onSubmit={handleSubmit(onValid)}>
-                <input
-                    {...register("Email", {
-                        required: "Email is required",
-                        minLength: 10,
-                        pattern: {
-                            value: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-                            message: "Only gmail.com is allowed.",
-                        },
-                    })}
-                    placeholder="Email"
+            <form onSubmit={handleSubmit(handleValid)}>
+                <input {...register("toDo", {
+                    required: "Please write to-do-list"
+                })}
+                    placeholder="Write to do"
                 />
-                <span>{errors?.Email?.message}</span>
-
-                <input
-                    {...register("LastName", {
-                        required: "LastName is required",
-                    })}
-                    placeholder="LastName"
-                />
-                <span>{errors?.LastName?.message}</span>
-
-                <input
-                    {...register("FirstName", {
-                        required: "FirstName is required",
-                        validate: {
-                            noKim:(value)=>
-                            value.includes("Kim") ? "No more Kim family" : true, //true면 통과 false면 에러 호출
-                            noBenz: (value) => value.includes("Benz") ? "Just No" :true,
-                        }
-                        })}
-                    placeholder="FirstName"
-                />
-                <span>{errors?.FirstName?.message}</span>
-
-                <input
-                    {...register("Password1", {
-                        required: "Password1 is required",
-                        minLength: {
-                            value: 3,
-                            message: "Password1 is too short.",
-                        },
-                    })}
-                    placeholder="Password"
-                />
-                <span>{errors?.Password1?.message}</span>
-
-                <input
-                    {...register("Password2", {
-                        required: "Password2 is required",
-                        minLength: {
-                            value: 3,
-                            message: "Password2 is too short.",
-                        },
-                    })}
-                    placeholder="Password2"
-                />
-                <span>{errors?.Password2?.message}</span>
                 <button>Add</button>
-                <span>{errors?.extraError?.message}</span>
             </form>
         </div>
     );
 }
+
+// function ToDoList() {
+//     //handleSubmit is for validation and preventDefault
+//     const {
+//         register,
+//         handleSubmit,
+//         formState: { errors },
+//         setError,
+//     } = useForm<IForm>({
+//         defaultValues: {
+//             Email: "@gmail.com",
+//         },
+//     });
+//     const onValid = (data: IForm) => {
+//         if (data.Password1 !== data.Password2) {
+//             setError(
+//                 "Password1",
+//                 { message: "Password are not the same." },
+//                 { shouldFocus: true }
+//             ); //password1에서 에러 나면 커서가 이리고 옮겨진다.
+//             setError("extraError", { message: "Server offline" });
+//         }
+//     };
+//     console.log(errors);
+
+//     return (
+//         <div>
+//             <form
+//                 style={{ display: "flex", flexDirection: "column" }}
+//                 onSubmit={handleSubmit(onValid)}>
+//                 <input
+//                     {...register("Email", {
+//                         required: "Email is required",
+//                         minLength: 10,
+//                         pattern: {
+//                             value: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+//                             message: "Only gmail.com is allowed.",
+//                         },
+//                     })}
+//                     placeholder="Email"
+//                 />
+//                 <span>{errors?.Email?.message}</span>
+
+//                 <input
+//                     {...register("LastName", {
+//                         required: "LastName is required",
+//                     })}
+//                     placeholder="LastName"
+//                 />
+//                 <span>{errors?.LastName?.message}</span>
+
+//                 <input
+//                     {...register("FirstName", {
+//                         required: "FirstName is required",
+//                         validate: {
+//                             noKim:(value)=>
+//                             value.includes("Kim") ? "No more Kim family" : true, //true면 통과 false면 에러 호출
+//                             noBenz: (value) => value.includes("Benz") ? "Just No" :true,
+//                         }
+//                         })}
+//                     placeholder="FirstName"
+//                 />
+//                 <span>{errors?.FirstName?.message}</span>
+
+//                 <input
+//                     {...register("Password1", {
+//                         required: "Password1 is required",
+//                         minLength: {
+//                             value: 3,
+//                             message: "Password1 is too short.",
+//                         },
+//                     })}
+//                     placeholder="Password"
+//                 />
+//                 <span>{errors?.Password1?.message}</span>
+
+//                 <input
+//                     {...register("Password2", {
+//                         required: "Password2 is required",
+//                         minLength: {
+//                             value: 3,
+//                             message: "Password2 is too short.",
+//                         },
+//                     })}
+//                     placeholder="Password2"
+//                 />
+//                 <span>{errors?.Password2?.message}</span>
+//                 <button>Add</button>
+//                 <span>{errors?.extraError?.message}</span>
+//             </form>
+//         </div>
+//     );
+// }
 
 export default ToDoList;
